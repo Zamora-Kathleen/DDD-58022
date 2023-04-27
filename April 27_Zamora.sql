f0e42c7b746ea41e1bfece3e1f3b377cb36e3497
Zamora-Kathleen <98940694+Zamora-Kathleen@users.noreply.github.com>
2023-04-27 12:55:58 +0800

    Add files via upload

---
+++ April 27_Zamora.sql
@@ -0,0 +1,34 @@
+Create Database Customer;
+Create Table tableforCustomer(
+CustomerID INT Primary Key,
+CustomerName VarChar (50) NULL,
+Municipality VarChar(50),
+City VarChar(50) NULL,
+Salary_in_peso Float
+);
+
+Select * from tableforcustomer;
+
+Insert into tableforCustomer VALUES(
+CustomerID,
+CustomerName,
+Municipality,
+City,
+Salary_in_peso
+);
+
+Insert Into tableforCustomer Values (
+'1', 'Gina de Leon', 'Apalit', 'Pampanga', '5000'),
+('2', 'Amara Luna', 'Mexico', 'Pampanga', '6000'),
+('3', 'Lucila Maulon' , 'Angat', 'Bulacan', '1000'),
+('4', ' Rafael Santos', 'Lumban', 'Laguna', '4500'),
+('5', 'Maricel Moran', 'Calumpit', 'Bulacan', '12000'),
+('6', 'Antonio Morena', 'Santa Maria', 'Bulacan', '8500'),
+('7','Hannah Moos','Alaminos','Laguna','6000'),
+('8','Fred Grogorio','Lumban','laguna','5000'),
+('9','Maria Andres','Porac','Pampanga','1800'),
+('10','Liza Ramos','Alaminos','Laguna','14000');
+
+-- show tablenmaed tablecustomer and its data
+
+Select * from tableforcustomer;
